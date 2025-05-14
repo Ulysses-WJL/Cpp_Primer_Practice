2b1451becda6169bb1776e2e7f0ac59dc73d780c
Ulysses <38154485+Ulysses-WJL@users.noreply.github.com>
2025-05-14 17:40:39 +0800

    Create chapter09.cpp

---
+++ cpp_source/ch09/chapter09.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include "chapter09.h"
+
+using std::vector;
+using std::list;
+using std::deque;
+
+
+
+void q_9_2() {
+    list<deque<int>> l;
+}
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
