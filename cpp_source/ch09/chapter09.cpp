dd1cf049f6cd6e2de78dd9d7e7854a62f82be6dc
ulysses <jianliangwu1171@gmail.com>
2025-05-14 21:37:25 +0800

    0514

--- cpp_source/ch09/chapter09.cpp
+++ cpp_source/ch09/chapter09.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <list>
 #include <deque>
-#include "chapter09.h"
 
 using std::vector;
 using std::list;
@@ -14,7 +13,28 @@ void q_9_2() {
     list<deque<int>> l;
 }
 
-int main(int argc, char **argv) {
+bool q_9_3(vector<int>::const_iterator beg, vector<int>::const_iterator end, const int val) {
+    while (beg != end) {
+        if (*beg++ == val) {
+            return true;
+        }
+    }
+    return false;
+}
 
+vector<int>::const_iterator q_9_4(vector<int>::const_iterator beg, vector<int>::const_iterator end, const int val) {
+    while (beg != end) {
+        if (*beg == val) {
+            break;
+        }
+        ++beg;
+    }
+    return beg;
+}
+
+int main(int argc, char **argv) {
+    vector<int> vec{1, 2, 3, 4, 5};
+    auto flag = q_9_3(vec.begin(), vec.end(), 1);
+    std::cout << flag << std::endl;
     return 0;
 }
