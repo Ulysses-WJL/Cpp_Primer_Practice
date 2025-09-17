a94f69306da4cbfcc04a8166dc46f8046b9d0cde
Ulysses <38154485+Ulysses-WJL@users.noreply.github.com>
2025-09-17 15:13:43 +0800

    Update Bulk_quote.h

--- cpp_source/ch15/Bulk_quote.h
+++ cpp_source/ch15/Bulk_quote.h
@@ -11,6 +11,7 @@ public:
     Bulk_quote() = default;
     Bulk_quote(const std::string& , double, std::size_t, double);
     double net_price(std::size_t) const override;
+    void debug() override {std::cout << "Bulk_quote" << std::endl;}
 private:
     std::size_t min_qty = 0;
     double discount = 0.0;
