8a93be5a1bb1be8cfcdfb3cf680d7ec55f7e6b9d
ulysses <jianliangwu1171@gmail.com>
2025-09-20 16:15:50 +0800

    0920 class scope

--- cpp_source/ch15/chapter15.cpp
+++ cpp_source/ch15/chapter15.cpp
@@ -228,6 +228,7 @@ void q_15_17() {
 }
 
 class Base1 {
+    friend class Pal;
 protected:
     int prot_mem; // protected member
 };
@@ -243,8 +244,236 @@ void clobber(Sneaky &s) { s.j = s.prot_mem = 0; }
 // clobber 不是Base1的友元
 // void clobber(Base1 &b) {b.prot_mem = 0;} // Member is inaccessible
 
-int main(int argc, char *argv[]) {
+// 派生类能否访问基类的member，由基类的access specifier控制
+class Base2 {
+public:
+    void pub_mem() {}; // public member
+protected:
+    int prot_mem; // protected member
+private:
+    char priv_mem; // private member
+};
 
+
+struct Pub_Derv: public Base2 {
+    int f() {return prot_mem;}
+    // char g() {return priv_mem;}
+    void memfcn(Base2 &b) { b = *this; }
+};
+
+// derivation access specifier 控制派生类如何使用
+// 继承自基类的member
+struct Priv_Derv : private Base2 {
+    // private derivation doesn’t affect access in the derived class
+    int f1() const { return prot_mem; }
+    void memfcn(Base2 &b) { b = *this; }
+};
+
+void test_inheritance() {
+    Pub_Derv pub;
+    Priv_Derv priv;
+    pub.pub_mem();
+    // priv.pub_mem();  // pub_mem is private in the derived class
+}
+
+
+// friendship 无法转换或继承
+class Pal {
+public:
+    int f(Base1 b) {return b.prot_mem;}
+    // Pal 不是Sneaky的友元，无法访问Sneaky的private成员j
+    // int f2(Sneaky s) {return s.j;}
+    // Pal是Base1的友元，可以访问Base1的protect成员prot_mem, 包括对嵌入在派生自Base的类型的对象中的Base对象的访问
+    int f3(Sneaky s) {return s.prot_mem;}
+};
+
+// Exempting Individual Members
+class Base3 {
+public:
+    std::size_t size() const {return n;}
+protected:
+    std::size_t n = 0;
+};
+
+// 使用using 后 访问控制由using前的 access specifier 控制
+class Derived3 : private Base3 {
+public:
+    // maintain access levels for members related to the size of the object
+    using Base3::size;
+protected:
+    using Base3::n;
+};
+
+void test_exempting_individual_members() {
+    Derived3 derived;
+    cout << derived.size() << endl;
+}
+
+class Prot_derv: protected Base2 {
+    void memfcn(Base2 &b) { b = *this; }
+};
+
+class Derived_from_Public: public Pub_Derv {
+    void memfcn(Base2 &b) { b = *this; }
+};
+
+class Derived_from_Private: public Priv_Derv {
+    // 派生类D的派生类的成员函数和友元需要public、protect继承D才能使用 derived-to-base的转换；
+    // void memfcn(Base2 &b) { b = *this; }
+};
+
+class Derived_from_Protected: public Prot_derv {
+    void memfcn(Base2 &b) { b = *this; }
+};
+
+void q_15_18() {
+    /*
+    *
+    只有在派生类是使用public的方式继承基类时，user code 才可以使用派生类到基类（derived-to-base）的转换
+     */
+    Pub_Derv d1;
+    Priv_Derv d2;
+    Prot_derv d3;
+    Derived_from_Public dd1;
+    Derived_from_Private dd2;
+    Derived_from_Protected dd3;
+    Base2 *p = &d1; // d1 has type Pub_Derv  合法
+    // p = &d2; // d2 has type Priv_Derv  不合法
+    // p = &d3; // d3 has type Prot_Derv  不合法
+    p = &dd1; // dd1 has type Derived_from_Public  合法
+    // p = &dd2; // dd2 has type Derived_from_Private  不合法
+    // p = &dd3; // dd3 has type Derived_from_Protected  不合法
+}
+
+void q_15_19() {
+    /**
+     * 类成员函数中执行 dervied-to-base的conversion，
+    * 派生类D的成员函数和友元总是可以执行derived-to-base的转换；
+    * void memfcn(Base &b) { b = *this; }
+    派生类D的派生类的成员函数和友元需要public、protect继承D才能使用 derived-to-base的转换；
+     * 合法： Pub_Derv， Priv_Derv， Prot_Derv, Derived_from_Public, Derived_from_Protected
+     * 不合法： Derived_from_Private
+     */
+}
+
+// 15_21: 2Dshape
+
+
+class Shape {
+public:
+    typedef std::pair<double, double > Coordinate;
+    Shape() = default;
+    Shape(const string &n) : name(n) {};
+    virtual double perimeter() const = 0;  // 抽象基类，pure virtual function 不需要实现
+    virtual double area() const = 0;
+    virtual ~Shape() = default;
+
+private:
+    string name;
+};
+
+class Rectangle : public Shape {
+public:
+    Rectangle() = default;
+    Rectangle(
+        const string &n,
+        const Coordinate &d, const double w, const double h) : Shape(n), left_up(d), width(w), height(h) {};
+    double area() const override {return width * height;};
+    double perimeter() const override {return 2 * (width + height);};
+    ~Rectangle() override = default;
+protected:
+    Coordinate left_up;
+    double width;
+    double height;
+};
+
+class Square : public Rectangle {
+public:
+    Square() = default;
+    Square(const string &n, const Coordinate &d, double w): Rectangle(n, d, w, w) {};
+    ~Square() override = default;
+};
+
+void q_15_21() {
+    Square square("正方形", std::pair<double, double>(0.0, 0.0), 10.0);
+
+    cout << "area: " << square.area() << endl;
+    cout << "perimeter: " << square.perimeter() << endl;
+}
+
+// name collisions
+struct BaseName {
+    BaseName(): mem(0){};
+    int memfunc() {return mem;}
+protected:
+    int mem;
+};
+
+struct DerivedName : public BaseName {
+    DerivedName(int i): mem(i) {};
+    int get_mem() const {return  BaseName::mem;}  // 使用scope operator
+    int memfunc(int i) const {return mem;}
+protected:
+    int mem;
+};
+
+void test_name_collisions() {
+    DerivedName d(42);
+    BaseName b;
+    cout << d.get_mem() << endl;
+    b.memfunc();  // 基类的 memfunc
+    d.memfunc(10);  // 派生类的memfunc
+    // name lookup happens before type checking
+
+    // d.memfunc();  // 基类的memfunc被覆盖， 即时参数列表都不一样
+    d.BaseName::memfunc();
+}
+
+
+class BaseFunc {
+public:
+    virtual int fcn() {return 0;};
+};
+class D1Func : public BaseFunc {
+public:
+    // hides fcn in the base; this fcn is not virtual
+    // D1 inherits the definition of Base::fcn()
+    int fcn(int i) {return i;}; // parameter list differs from fcn in Base
+    virtual void f2() {}; // new virtual function that does not exist in Base
+};
+class D2Func : public D1Func {
+public:
+    int fcn(int i) {return i;}; // nonvirtual function hides D1::fcn(int)
+    int fcn() {return 1001;}; // overrides virtual fcn from Base
+    void f2() {}; // overrides virtual f2 from D1
+};
+
+void test_hidden_virtual_func() {
+    BaseFunc bobj; D1Func d1obj; D2Func d2obj;
+    // 使用基类的指针
+    BaseFunc *bp1 = &bobj, *bp2 = &d1obj, *bp3 = &d2obj;
+    bp1->fcn(); // virtual call, will call Base::fcn at run time
+    bp2->fcn(); // virtual call, will call Base::fcn at run time
+    bp3->fcn(); // virtual call, will call D2::fcn at run time
+    D1Func *d1p = &d1obj; D2Func *d2p = &d2obj;
+    // derived-to-base, 基类中没有f2
+    // bp2->f2(); // error: Base has no member named f2
+    d1p->f2(); // virtual call, will call D1::f2() at run time
+    d2p->f2(); // virtual call, will call D2::f2() at run time
+    BaseFunc *p1 = &d2obj;
+    D1Func *p2 = &d2obj;
+    D2Func *p3 = &d2obj;
+    // p1->fcn(42);  // base 中的fcn 是无参数的
+
+
+    p2->fcn(42);
+    p3->fcn(42);
+}
+
+
+int main(int argc, char *argv[]) {
+    test_name_collisions();
+    q_15_21();
     q_15_17();
     q_15_14();
     q_15_13();
